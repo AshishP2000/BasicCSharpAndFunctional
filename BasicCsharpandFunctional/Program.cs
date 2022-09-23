d5ed0e43a5ca6a0e8bf2630e2f82b498492fbb96
Ashish <ashish2000patil@gmail.com>
2022-09-23 06:59:04 +0530

    [Ashish]Added: Swap Two Numbers

---
+++ BasicCsharpandFunctional/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCsharpandFunctional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+            Console.WriteLine("Enter number to choose operation: ");
+            Console.WriteLine("1.Swap Numbers");
+            choice = Convert.ToInt32(Console.ReadLine());
+            
+            switch (choice)
+            {
+                case 1:
+                    //int a = 0, b = 0;
+                    //a = Convert.ToInt32(Console.ReadLine());
+                    //b = Convert.ToInt32(Console.ReadLine());
+                    SwapNumbers.Swap();
+                    break;
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BasicCsharpandFunctional/SwapNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCsharpandFunctional
+{
+    public class SwapNumbers
+    {
+        public static void Swap()
+        {
+            int temp=0;
+            int a = 0, b = 0;
+            Console.WriteLine("Enter Two Numbers: ");
+            Console.Write("a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Before Swapping a= {0} b= {1}",a,b);
+            temp = a;
+            a=b;
+            b=temp;
+
+            Console.WriteLine("After Swapping a= {0} b= {1}", a, b);
+        }
+    }
+}
